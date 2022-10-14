732355dbb39f8021bfdb2e7039929b672a0bc281
realjioke <scarlobest@gmail.com>
2022-10-14 15:18:33 -0700

    100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,30 +1,37 @@
-#include <stdlib.h>
-#include <time.h>
 #include <stdio.h>
 /**
  *main-program entry point.
- *
- *Return:0 if no error, non-zero value if error.
+ *Return:0 success, non zero fail
  */
 int main(void)
 {
-	int n;
-	int ldigit;
+	int i, k = '0', j;
 
-	srand(time(0));
-	n = rand() - RAND_MAX / 2;
-	ldigit = n % 10;
-	if (ldigit > 5)
+	for (i = '0'; i <= '9'; i++)
 	{
-		printf("Last digit of %d is %d and is greater than 5\n", n, ldigit);
+		for (j = k; j <= '9'; j++)
+		{
+			if (i != j)
+			{
+				putchar(i);
+				putchar(j);
+			}
+			if (i == j)
+			{
+				continue;
+			}
+			if (i == '8' && j == '9')
+			{
+				break;
+			}
+			else
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+		k++;
 	}
-	else if (ldigit == 0)
-	{
-		printf("Last digit of %d is %d and is 0\n", n, ldigit);
-	}
-	else if (ldigit < 6 && ldigit != 0)
-	{
-		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, ldigit);
-	}
+	putchar('\n');
 	return (0);
 }
